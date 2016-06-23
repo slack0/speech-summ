c13e1596d4cddb0e6cc49acd588e5966d245215b
Swamy Muddu <smuddu@gmail.com>
2016-06-23 00:18:25 -0700

    Sync commit ...

--- prototyping/init_model/extract_topics.py
+++ prototyping/init_model/extract_topics.py
@@ -12,17 +12,13 @@ import unidecode
 import operator
 import pprint
 
+from collections import defaultdict
 
+from textblob import TextBlob
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem.porter import PorterStemmer
 from sklearn.decomposition import NMF
 
-
-path = '/Users/smuddu/galvanize/capstone/data/Speeches/Obama'
-token_dict = {}
-stemmer = PorterStemmer()
-
-pp = pprint.PrettyPrinter(indent=2)
 
 def stem_tokens(tokens, stemmer):
     stemmed = []
@@ -36,9 +32,12 @@ def tokenize(text):
     #stems = stem_tokens(tokens, stemmer)
     return tokens
 
-for subdir, dirs, files in os.walk(path):
-    for file in files:
-        if (file != 'links'):
+
+def extract_topics(corpus_path, num_topics):
+
+    ### construct a token dictionary
+    for subdir, dirs, files in os.walk(corpus_path):
+        for file in files:
             pp.pprint("-- processing: {}".format(file))
             file_path = subdir + os.path.sep + file
             shakes = open(file_path, 'r')
@@ -48,25 +47,59 @@ for subdir, dirs, files in os.walk(path):
             lowers = text.replace('\n',' ').replace('\r',' ')
             while "  " in lowers:
                 lowers = lowers.replace('  ',' ')
+
+            ''' store raw text -- for sentence extraction '''
+            raw_text[file] = lowers
+
+            ''' store no_punctuation for NMF '''
             no_punctuation = lowers.translate(None, string.punctuation)
             token_dict[file] = no_punctuation
 
+    ### TF-IDF
+    tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+    tfs = tfidf.fit_transform(token_dict.values())
 
-tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
-tfs = tfidf.fit_transform(token_dict.values())
+    model = NMF(n_components=num_topics, init='random', random_state=0)
+    Wmat = model.fit_transform(tfs)
+
+    id2word = {}
+    for k in tfidf.vocabulary_.keys():
+        id2word[tfidf.vocabulary_[k]] = k
+
+    for topic_index in xrange(num_topics):
+        print ""
+        pp.pprint("-- Top words in topic:")
+        topic_importance = dict(zip(id2word.values(),list(model.components_[topic_index])))
+        sorted_topic_imp = sorted(topic_importance.items(), key=operator.itemgetter(1),reverse=True)
+        pp.pprint([i[0] for i in sorted_topic_imp[:10]])
 
 
-num_topics = 20
-model = NMF(n_components=num_topics, init='random', random_state=0)
-model.fit(tfs)
+    ### For each document/speech -- extract the top sentences based on their cosine similarity to topics discovered
 
-id2word = {}
-for k in tfidf.vocabulary_.keys():
-    id2word[tfidf.vocabulary_[k]] = k
+    speech_hash = defaultdict(dict)
+    for doc in token_dict.iterkeys():
+        doc_blob = TextBlob(raw_text[doc])
+        sentence_count = 0
+        for sentence in doc_blob.sentences:
+            speech_hash[doc][sentence_count] = sentence
+            sentence_count += 1
 
-for topic_index in xrange(num_topics):
-    print ""
-    pp.pprint("-- Top words in topic:")
-    topic_importance = dict(zip(id2word.values(),list(model.components_[topic_index])))
-    sorted_topic_imp = sorted(topic_importance.items(), key=operator.itemgetter(1),reverse=True)
-    pp.pprint([i[0] for i in sorted_topic_imp[:19]])
+        #print 'KEYS: ', speech_hash[doc].keys()
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    token_dict = {}
+    raw_text = {}
+
+    stemmer = PorterStemmer()
+    pp = pprint.PrettyPrinter(indent=2)
+
+    path = '/Users/smuddu/galvanize/capstone/data/Speeches/Obama'
+    extract_topics(path,20)
+    print len(token_dict)
